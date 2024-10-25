333d3d5b5205083278eae4877f1594d6b3213640
Aniket More <130898814+aniketmore-pixel@users.noreply.github.com>
2024-10-25 06:52:04 +0530

    Update App.jsx

--- A8/App.jsx
+++ A8/App.jsx
@@ -1,1 +1,29 @@
 import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [firstName, setFirstName] = useState('Aniket');
+    const [lastName, setLastName] = useState('More');
+    const [fullName, setFullName] = useState('');
+
+    useEffect(() => {
+        setFullName(`${firstName} ${lastName}`)
+    }, [firstName, lastName]);
+
+    return (
+        <div>
+            <h1>React Hooks</h1>
+            <br />
+            <label for="firstname">First Name</label>
+            <input type="text" id="firstname" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            <hr />
+            <label for="lastname">Last Name</label>
+            <input type="text" id="lastname" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+
+            <div>
+                <p>Full Name: {fullName}</p>
+            </div>
+        </div>
+
+
+    )
+}
